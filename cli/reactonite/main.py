8ae8f16b539350c9df1d2eb3aa3e8206f3b60e49
Aniket Pradhan <anipradh@adobe.com>
2020-10-31 13:55:20 +0530

    Fix project directories and paths

--- cli/reactonite/main.py
+++ cli/reactonite/main.py
@@ -15,22 +15,28 @@ def create_empty_project(project_name):
 
     # Create directory project_name/src
     project_dir = os.path.join(".", project_name)
-    react_working_dir = os.path.join(project_dir, "react_src")
-    static_dir = os.path.join(project_dir, "static")
+    dist_dir = os.path.join(project_dir, "dist")
+    src_dir = os.path.join(project_dir, "src")
 
-    # Path to index.html
-    html_file_path = os.path.join(project_dir, "index.html")
+    static_dir = os.path.join(src_dir, "static")
+    html_file_path = os.path.join(src_dir, "index.html")
+
+    config_file_path = os.path.join(project_dir, "config.json")
 
     # Create working dir and static dir
-    os.makedirs(react_working_dir)
+    os.makedirs(dist_dir)
+    os.makedirs(src_dir)
     os.makedirs(static_dir)
 
-    # Create template index.html in project dir
+    # Create template index.html in src dir
     open(html_file_path, 'a').close()
+
+    # Create template config.json in project dir
+    open(config_file_path, 'a').close()
 
     # Create npm project
     npm = node_wrapper(project_name,
-                       working_dir=react_working_dir)
+                       working_dir=dist_dir)
     npm.create_react_app()
 
     # Transpile once

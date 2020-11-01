d61130c65015c464d17476c4d5645c23bdeac063
Chirag Jain <jainchg@gmail.com>
2020-11-01 14:53:24 +0530

    Create config.py to store defaults

---
+++ cli/reactonite/config.py
@@ -0,0 +1,6 @@
+class DEFAULTS:
+    SRC_DIR = 'src'
+    DEST_DIR = 'dist'
+    STATIC_DIR = 'static'
+    HTML_FILE_PATH = 'index.html'
+    CONFIG_FILE_PATH = 'config.json'
--- cli/reactonite/main.py
+++ cli/reactonite/main.py
@@ -1,5 +1,6 @@
 import os
 import click
+from reactonite.config import DEFAULTS
 
 from reactonite.node_wrapper import node_wrapper
 
@@ -13,26 +14,33 @@ def cli():
 @click.argument('project-name')
 def create_empty_project(project_name):
 
+    #TODO: Check if project_name is valid
+
     # Create directory project_name/src
     project_dir = os.path.join(".", project_name)
-    dist_dir = os.path.join(project_dir, "dist")
-    src_dir = os.path.join(project_dir, "src")
+    dist_dir = os.path.join(project_dir, DEFAULTS.DEST_DIR)
+    src_dir = os.path.join(project_dir, DEFAULTS.SRC_DIR)
 
-    static_dir = os.path.join(src_dir, "static")
-    html_file_path = os.path.join(src_dir, "index.html")
+    static_dir = os.path.join(src_dir, DEFAULTS.STATIC_DIR)
+    html_file_path = os.path.join(src_dir, DEFAULTS.HTML_FILE_PATH)
 
-    config_file_path = os.path.join(project_dir, "config.json")
+    config_file_path = os.path.join(project_dir, DEFAULTS.CONFIG_FILE_PATH)
 
     # Create working dir and static dir
     os.makedirs(dist_dir)
+    # TODO: Check if directory created
     os.makedirs(src_dir)
+    # TODO: Check if directory created
     os.makedirs(static_dir)
+    # TODO: Check if directory created
 
     # Create template index.html in src dir
     open(html_file_path, 'a').close()
+    # TODO: Check if file created
 
     # Create template config.json in project dir
     open(config_file_path, 'a').close()
+    # TODO: Check if file created
 
     # Create npm project
     npm = node_wrapper(project_name,

810c2350e3f0e198e1e73dea1eaf730da0fd77de
Chirag Jain <jainchg@gmail.com>
2020-11-02 21:33:40 +0530

    Add constants for file names

--- core/main.py
+++ core/main.py
@@ -1,49 +1,46 @@
 import os.path
 import shutil
 from bs4 import BeautifulSoup
+from core.config import DEFAULTS
 
-PATH_INPUT_FOLDER = "src"
-NAME_STATIC_FOLDER = "static"
-PATH_STATIC_FOLDER = os.path.join(PATH_INPUT_FOLDER, NAME_STATIC_FOLDER)
-ENTRY_POINT_FILENAME = "App.html"
-ENTRY_POINT_FILE_PATH = os.path.join(PATH_INPUT_FOLDER, ENTRY_POINT_FILENAME)
-
-PATH_OUTPUT_FOLDER = "dist/src/reactonite-code"
-PATH_STATIC_FOLDER_OUTPUT = os.path.join(PATH_OUTPUT_FOLDER, NAME_STATIC_FOLDER)
+SRC_STATIC_DIR_PATH = os.path.join(DEFAULTS.SRC_DIR, DEFAULTS.STATIC_DIR)
+SRC_ENTRY_POINT_PATH = os.path.join(DEFAULTS.SRC_DIR, DEFAULTS.ENTRY_POINT_FILE)
+DEST_STATIC_DIR_PATH = os.path.join(DEFAULTS.DEST_DIR, DEFAULTS.STATIC_DIR)
 
 class Reactonite:
 
     def __init__(self):
-        self.PARSER = "html.parser"
+        pass
 
     def __cleanBuildFolderIfExists(self):
-        if(os.path.isdir(PATH_OUTPUT_FOLDER)):
-            for root, dirs, files in os.walk(PATH_OUTPUT_FOLDER):
+        if(os.path.isdir(DEST_STATIC_DIR_PATH)):
+            print('Build folder exists at', DEST_STATIC_DIR_PATH, '. cleaning!')
+            for root, dirs, files in os.walk(DEST_STATIC_DIR_PATH):
                 for innerDir in dirs:
                     shutil.rmtree(os.path.join(root, innerDir))
                 for innerFile in files:
                     os.remove(os.path.join(root, innerFile))
         else:
-            os.mkdir(PATH_OUTPUT_FOLDER)
+            os.mkdir(DEST_STATIC_DIR_PATH)
 
     def __copyStaticFolderToBuild(self):
-        shutil.copytree(PATH_STATIC_FOLDER, PATH_STATIC_FOLDER_OUTPUT)
+        print('Copying static folder to build directory')
+        shutil.copytree(SRC_STATIC_DIR_PATH, DEST_STATIC_DIR_PATH)
 
     def __transpileFile(self, filepath, verbose):
-
         _, filename = os.path.split(filepath)
         filenameWithNoExtension, file_extension = os.path.splitext(filename)
 
         if file_extension != ".html":
-            return
+            raise Exception(filename, 'is not a HTML file')
 
         if verbose:
             print("Transpiling file: " + filename)
     
         with open(filepath, 'r') as index:
-            soup = BeautifulSoup(index, 'html.parser')
+            soup = BeautifulSoup(index, DEFAULTS.BS_PARSER)
 
-        with open(os.path.join(PATH_OUTPUT_FOLDER, filenameWithNoExtension + ".js"), 'w') as outfile:
+        with open(os.path.join(DEFAULTS.DEST_DIR, filenameWithNoExtension + ".js"), 'w') as outfile:
             function = "function App() {return (<>" + soup.html.prettify() + "</>);}"
             outfile.write(
                 """
@@ -62,7 +59,7 @@ class Reactonite:
         self.__cleanBuildFolderIfExists()
         
         # Copy all static assets if exists 
-        if(os.path.isdir(PATH_STATIC_FOLDER)):
+        if(os.path.isdir(DEFAULTS.STATIC_DIR)):
             if verbose:
                 print("Coping static files...")
             self.__copyStaticFolderToBuild()
@@ -70,21 +67,22 @@ class Reactonite:
         # Transpile all files
         if verbose:
             print("Transpiling files...")
+
         # TODO: Loop through all files/dirs in src folder except static(NAME_STATIC_FOLDER) dir
-        filepath = os.path.join(PATH_INPUT_FOLDER, ENTRY_POINT_FILENAME)
+        filepath = SRC_ENTRY_POINT_PATH
         self.__transpileFile(filepath, verbose)
 
     def transpile(self, verbose=False):
         # Check if entry point folder exists
-        if(os.path.isdir(PATH_INPUT_FOLDER)):
+        if(os.path.isdir(DEFAULTS.SRC_DIR)):
             # Check if entry point file exists
-            if(os.path.isfile(ENTRY_POINT_FILE_PATH)):
+            if(os.path.isfile(SRC_ENTRY_POINT_PATH)):
                 # Initial checks are done run code
                 self.__transpileSrc(verbose=verbose)
             else:
-                raise OSError("Entry point file doesn't exists " + ENTRY_POINT_FILE_PATH)
+                raise Exception("Entry point file doesn't exist at ", SRC_ENTRY_POINT_PATH)
         else:
-            raise OSError("Entry point folder doesn't exists " + PATH_INPUT_FOLDER)
+            raise Exception("Entry point folder doesn't exist at ", DEFAULTS.SRC_DIR)
 
 if __name__ == "__main__":
     reactonite = Reactonite()

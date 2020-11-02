a3e485982da29763577a1844d41451c1a1bfe4f5
Chirag Jain <jainchg@gmail.com>
2020-11-02 21:01:55 +0530

    Add File handling exceptions in cli/main.py

--- cli/reactonite/main.py
+++ cli/reactonite/main.py
@@ -5,11 +5,19 @@ from reactonite.config import DEFAULTS
 from reactonite.node_wrapper import node_wrapper
 from reactonite.watcher import reactonite_watcher
 
+def create_dir(path):
+    os.makedirs(path)
+    if not os.path.isdir(path):
+        raise Exception('Folder can not be created at', path)
+
+def create_file(path):
+    open(path, 'w').close()
+    if not os.path.isfile(path):
+        raise Exception('File can not be created at', path)
 
 @click.group()
 def cli():
     pass
-
 
 @cli.command()
 @click.argument('project-name')
@@ -28,25 +36,20 @@ def create_empty_project(project_name):
     config_file_path = os.path.join(project_dir, DEFAULTS.CONFIG_FILE_PATH)
 
     # Create working dir and static dir
-    os.makedirs(project_dir)
-    os.makedirs(dist_dir)
-    # TODO: Check if directory created
-    os.makedirs(src_dir)
-    # TODO: Check if directory created
-    os.makedirs(static_dir)
-    # TODO: Check if directory created
+
+    create_dir(project_dir)
+    create_dir(dist_dir)
+    create_dir(src_dir)
+    create_dir(static_dir)
 
     # Create template index.html in src dir
-    open(html_file_path, 'a').close()
-    # TODO: Check if file created
+    create_file(html_file_path)
 
     # Create template config.json in project dir
-    open(config_file_path, 'a').close()
-    # TODO: Check if file created
+    create_file(config_file_path)
 
     # Create npm project
-    npm = node_wrapper(project_name,
-                       working_dir=dist_dir)
+    npm = node_wrapper(project_name, working_dir=dist_dir)
     npm.create_react_app()
 
     # Transpile once

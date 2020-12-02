b35026d206bc20416ca20d7ec72e8f1c888a11e1
Aniket Pradhan <aniket17133@iiitd.ac.in>
2020-12-02 13:07:48 +0530

    Fix tests for the updates.

--- tests/file_vars.py
+++ tests/file_vars.py
@@ -6,18 +6,22 @@ minimal_working_example = """<head>Test header</head>
 </body>
 """
 
-minimal_working_example_js = """
-        import React from 'react';
-        import Helmet from 'react-helmet';
-        
+minimal_working_example_js = """import React from "react";
+import Helmet from "react-helmet";
 
-        function App() {return (<><Helmet>
- Test header
-</Helmet>
-<h1>Heading</h1><p>Parargraph</p><a href="https://google.com">Link</a></>);}
+function App() {
+  return (
+    <>
+      <Helmet>Test header</Helmet>
+      <h1>Heading</h1>
+      <p>Parargraph</p>
+      <a href="https://google.com">Link</a>
+    </>
+  );
+}
 
-        export default App;
-        """
+export default App;
+"""
 
 full_working_example = """<!doctype html>
 <html lang="en">
@@ -49,30 +53,41 @@ full_working_example = """<!doctype html>
 </html>
 """
 
-full_working_example_js = """
-        import React from 'react';
-        import Helmet from 'react-helmet';
-        
+full_working_example_js = """import React from "react";
+import Helmet from "react-helmet";
 
-        function App() {return (<><Helmet>
- <meta charSet="utf-8"/>
- <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport"/>
- <link rel="stylesheet" src="./static/main.css"/>
- <title>
-  Reactonite App
- </title>
-</Helmet>
-<div className="main">
-<header className="main-header">
-<img alt="Reactonite" className="logo" src="./static/logo.png"/>
-<p>
-        Entry point is <code>src/index.html</code>, save to auto reload.
-      </p>
-<a className="link" href="https://github.com/SDOS2020/Team_3_Reactonite/" rel="noopener noreferrer" target="_blank">
-        Learn about Reactonite
-      </a>
-</header>
-</div><script src="./static/main.js"></script></>);}
+function App() {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          name="viewport"
+        />
+        <link rel="stylesheet" src="./static/main.css" />
+        <title>Reactonite App</title>
+      </Helmet>
+      <div className="main">
+        <header className="main-header">
+          <img alt="Reactonite" className="logo" src="./static/logo.png" />
+          <p>
+            Entry point is <code>src/index.html</code>, save to auto reload.
+          </p>
+          <a
+            className="link"
+            href="https://github.com/SDOS2020/Team_3_Reactonite/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Learn about Reactonite
+          </a>
+        </header>
+      </div>
+      <script src="./static/main.js"></script>
+    </>
+  );
+}
 
-        export default App;
-        """
+export default App;
+"""

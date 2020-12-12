3e2bf0074648b892cbbcebd178be0ac84ea16fef
avisionx <avi.grg4567@gmail.com>
2020-12-12 23:23:02 +0530

    add mobile device toggle navbar template

--- reactonite/init_src_dir/main.js
+++ reactonite/init_src_dir/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World! From Main.js");
+function toggleNavBar() {
+  var navbar = document.getElementById("navbar-collapse");
+  if (navbar.classList.contains("show")) {
+    navbar.classList.remove("show");
+  } else {
+    navbar.classList.add("show");
+  }
+}
+
+document
+  .getElementsByClassName("navbar-toggle")[0]
+  .addEventListener("click", toggleNavBar);

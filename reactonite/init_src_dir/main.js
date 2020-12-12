656984eaaa5f8ec62b69b04c698fb15c9573f8b3
avisionx <avi.grg4567@gmail.com>
2020-12-12 23:30:47 +0530

    update nav toggle location

--- reactonite/init_src_dir/main.js
+++ reactonite/init_src_dir/main.js
@@ -1,12 +1,1 @@
-function toggleNavBar() {
-  var navbar = document.getElementById("navbar-collapse");
-  if (navbar.classList.contains("show")) {
-    navbar.classList.remove("show");
-  } else {
-    navbar.classList.add("show");
-  }
-}
-
-document
-  .getElementsByClassName("navbar-toggle")[0]
-  .addEventListener("click", toggleNavBar);
+console.log("Hello World! From Main.js");

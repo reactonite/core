c9b649230f5ca012fee00729eba4dd897d82409f
amogh <amogh7777@gmail.com>
2020-12-22 03:01:23 +0530

    write files

--- reactonite/CreateHtmlCss.py
+++ reactonite/CreateHtmlCss.py
@@ -9,9 +9,13 @@ def saveHtml(content, loc):
     loc : str
         Location where we have to save the HTML File
     """
-    print('HTML')
-    print(content)
-    pass
+    html_front = '<!doctype html><html lang="en"><head><meta charset="utf-8"><title>The HTML5 Herald</title><meta name="description" content="The HTML5 Herald"><meta name="author" content="SitePoint"><link rel="stylesheet" href="main.css"></head><body><script src="js/scripts.js"></script>'
+    html_back = '</body></html>'
+    html_content = html_front + content + html_back
+    text_file = open(loc+"index.html", "w")
+    text_file.write(html_content)
+    text_file.close()
+    print("HTML")
 
 
 def saveCss(content, loc):
@@ -25,6 +29,7 @@ def saveCss(content, loc):
     loc : str
         Location where we have to save the CSS File
     """
+    text_file = open(loc+"main.css", "w")
+    text_file.write(content)
+    text_file.close()
     print('CSS')
-    print(content)
-    pass

5063c3c3697de428233efb789c29da46b911d5ff
avisionx <avi.grg4567@gmail.com>
2020-12-22 03:30:50 +0530

    update refs for main.css

--- reactonite/CreateHtmlCss.py
+++ reactonite/CreateHtmlCss.py
@@ -1,3 +1,6 @@
+import os
+
+
 def saveHtml(content, loc):
     """
     Takes body of the POST response from GrapesJS and saves HTML File
@@ -9,13 +12,13 @@ def saveHtml(content, loc):
     loc : str
         Location where we have to save the HTML File
     """
-    html_front = '<!doctype html><html lang="en"><head><meta charset="utf-8"><title>The HTML5 Herald</title><meta name="description" content="The HTML5 Herald"><meta name="author" content="SitePoint"><link rel="stylesheet" href="main.css"></head><body><script src="js/scripts.js"></script>'
+
+    html_front = '<!doctype html><html lang="en"><head><meta charset="utf-8"/><title>The HTML5 Herald</title><meta name="description" content="The HTML5 Herald"/><meta name="author" content="SitePoint"/><link rel="stylesheet" href="./main.css"/></head><body>'
     html_back = '</body></html>'
     html_content = html_front + content + html_back
-    text_file = open(loc+"index.html", "w")
+    text_file = open(os.path.join(loc, 'index.html'), "w")
     text_file.write(html_content)
     text_file.close()
-    print("HTML")
 
 
 def saveCss(content, loc):
@@ -29,7 +32,7 @@ def saveCss(content, loc):
     loc : str
         Location where we have to save the CSS File
     """
-    text_file = open(loc+"main.css", "w")
+
+    text_file = open(os.path.join(loc, 'main.css'), "w")
     text_file.write(content)
     text_file.close()
-    print('CSS')
